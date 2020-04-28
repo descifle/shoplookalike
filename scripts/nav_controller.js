e123ca2121ac1875f4b47ff44ead0da9eca08cdf
Descifle <descifle_itb@yahoo.com>
2020-04-27 20:23:07 -0400

    updated mobile compatabilities

--- scripts/nav_controller.js
+++ scripts/nav_controller.js
@@ -1,10 +1,16 @@
 const modContainer = document.querySelector('.nav_modal_container')
 const mobiNav = document.querySelector('.mobile_nav')
 const blockView = document.querySelector('.overlay')
+const closeButton = document.querySelector('.close-btn')
 const body = document.querySelector('body')
 
-modContainer.addEventListener('click' , (e) => {
-    blockView.style.visibility = "visible"
+modContainer.addEventListener('click' , () => {
+    blockView.classList.remove('mobile_cover')
     mobiNav.classList.add('interesting')
     // body.classList.add('stopscroll')
+})
+
+closeButton.addEventListener('click', () => {
+    mobiNav.classList.toggle('interesting')
+    blockView.classList.add('mobile_cover')
 })
